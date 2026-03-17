9f0fa1a36f2db8abda5ba4450d292070a42d8141
Calin <calinpeters98@hotmail.nl>
2026-03-17 20:41:53 +0100

    change log message

--- vinyl-collection-tool/main.py
+++ vinyl-collection-tool/main.py
@@ -2,13 +2,15 @@
 import os
 
 from app.src.utils import logger
-from app.src.creators.collection_creator import CollectionCreator
+from app.src.creators.collection_creator import DiscogsCollectionCreator
 from app.src.providers.discogs_provider import DiscogsProvider
 
 
 if __name__ == "__main__":
     proxy = DiscogsProvider('LPShuffler/0.1', os.getenv("DISCOGS_TOKEN"))
-    collection_creator = CollectionCreator(proxy)
+    collection_creator = DiscogsCollectionCreator(proxy)
     coll = collection_creator.create_collection()
     random_album = coll.random
-    logger.info(f"Random album: {random_album.artist} - {random_album.title}")
+    logger.info(f"Random album:\n\
+                {random_album.artist} - {random_album.title}\n\
+                Format: {random_album.format}")
