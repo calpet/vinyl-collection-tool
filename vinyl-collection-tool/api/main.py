c79f481f30a9f6f4de9c0bfaa60a9e1a556e4fb9
Calin <calinpeters98@hotmail.nl>
2026-03-18 21:22:36 +0100

    temporarily moved proxy instantiation to startup event; general cleanup

--- vinyl-collection-tool/api/main.py
+++ vinyl-collection-tool/api/main.py
@@ -1,5 +1,7 @@
 """Main API entry point for the vinyl collection tool."""
 
+from http import HTTPStatus
+
 from fastapi import FastAPI
 import os
 from app.providers.discogs_provider import DiscogsProvider
@@ -7,14 +9,21 @@ from app.creators.collection_creator import DiscogsCollectionCreator
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    proxy = DiscogsProvider('LPShuffler/0.1', os.getenv("DISCOGS_TOKEN"))
+    collection_creator = DiscogsCollectionCreator(proxy)
+    app.state.collection = collection_creator.create_collection()
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/favicon.ico")
+def read_favicon():
+    return HTTPStatus.NO_CONTENT
+
 @app.get("/random")
 def get_random_album():
-    proxy = DiscogsProvider('LPShuffler/0.1', os.getenv("DISCOGS_TOKEN"))
-    collection_creator = DiscogsCollectionCreator(proxy)
-    coll = collection_creator.create_collection()
-    random_album = coll.random
+    random_album = app.state.collection.random
     return {"album": random_album}
--- vinyl-collection-tool/app/creators/collection_creator.py
+++ vinyl-collection-tool/app/creators/collection_creator.py
@@ -1,5 +1,5 @@
 """Defines the CollectionCreator class responsible for creating a Collection instance from the data retrieved via the DiscogsProvider."""
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from app.utils import logger
 
@@ -10,6 +10,7 @@ from app.providers.discogs_provider import DiscogsProvider
 class CollectionCreator(ABC):
     """Abstract class for creating a Collection instance from the data retrieved."""
     
+    @abstractmethod
     def create_collection(self) -> Collection:
         """Creates and returns a Collection instance containing the albums from the user's collection."""
         pass
@@ -23,8 +24,8 @@ class DiscogsCollectionCreator(CollectionCreator):
         self._releases = proxy.get_releases()
         self._all_pages = proxy.get_pages()
 
-    def _create_album_list(self) -> list[Album]:
-        """Creates a list of Album instances from the data retrieved from the Discogs API."""
+    def create_collection(self) -> Collection:
+        """Creates and returns a Collection instance containing the albums from the user's collection."""
         albums = []
         for page in self._all_pages:
             for item in page:
@@ -33,9 +34,4 @@ class DiscogsCollectionCreator(CollectionCreator):
                               format=item.data["basic_information"]["formats"][0]["name"])
                 logger.debug(f"Created album: {album.artist} - {album.title} ({album.format})")
                 albums.append(album)
-        return albums
-
-    def create_collection(self) -> Collection:
-        """Creates and returns a Collection instance containing the albums from the user's collection."""
-        albums = self._create_album_list()
         return Collection(albums)
