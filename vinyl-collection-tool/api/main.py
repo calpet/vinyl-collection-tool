b76c73e4f1663c12acb7bd0463e4decdbe997ff2
Calin <calinpeters98@hotmail.nl>
2026-03-18 20:40:34 +0100

    added test endpoint to get random album

--- vinyl-collection-tool/api/main.py
+++ vinyl-collection-tool/api/main.py
@@ -1,9 +1,20 @@
 """Main API entry point for the vinyl collection tool."""
 
 from fastapi import FastAPI
+import os
+from app.providers.discogs_provider import DiscogsProvider
+from app.creators.collection_creator import DiscogsCollectionCreator
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/random")
+def get_random_album():
+    proxy = DiscogsProvider('LPShuffler/0.1', os.getenv("DISCOGS_TOKEN"))
+    collection_creator = DiscogsCollectionCreator(proxy)
+    coll = collection_creator.create_collection()
+    random_album = coll.random
+    return {"album": random_album}
--- vinyl-collection-tool/app/providers/discogs_provider.py
+++ vinyl-collection-tool/app/providers/discogs_provider.py
@@ -1,7 +1,7 @@
 """Defines the DiscogsProvider class that interacts with the Discogs API to retrieve the user's album collection."""
 
 from discogs_client import Client
-from utils import logger
+from app.utils import logger
 
 
 class DiscogsProvider:
