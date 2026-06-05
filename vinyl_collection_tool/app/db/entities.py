f37eca5d2308a1336ba85bf7f4bcb038552b00f2
Calin <calinpeters98@hotmail.nl>
2026-06-05 20:38:39 +0200

    added more entities

--- vinyl_collection_tool/app/db/entities.py
+++ vinyl_collection_tool/app/db/entities.py
@@ -1,6 +1,8 @@
 """SQLModel entities and junctions for the vinyl collection tool."""
 
 from typing import Optional, List
+from sqlalchemy import Column, JSON
+
 from sqlmodel import SQLModel, Field, Relationship
 
 
@@ -68,6 +70,24 @@ class Album(SQLModel, table=True):
     collections: List["Collection"] = Relationship(
         back_populates="albums", link_model=AlbumInCollectionLink
     )
+    vectors: List["AlbumVector"] = Relationship(back_populates="album")
+
+
+class VectorSchema(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    style_weight: Optional[int] = None
+    genre_weight: Optional[int] = None
+    release_year_weight: Optional[int] = None
+    album_vectors: List["AlbumVector"] = Relationship(back_populates="vector_schema")
+
+
+class AlbumVector(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    album_id: Optional[int] = Field(default=None, foreign_key="album.id")
+    vector_schema_id: Optional[int] = Field(default=None, foreign_key="vector_schema.id")
+    vector: Optional[List[float]] = Field(default=None, sa_column=Column(JSON))
+    album: Optional["Album"] = Relationship(back_populates="vectors")
+    vector_schema: Optional["VectorSchema"] = Relationship(back_populates="album_vectors")
 
 
 class Genre(SQLModel, table=True):
